5b8cfec3b29a8411db425344efa3582a8f3eca4e
V5623 <V5623@lab.aca>
2019-05-07 16:04:25 -0500

    Servicio REST

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 var cors = require('cors')
 
+function aletaorio(min, max){
+	return Math.floor(Math.random() * max) + min;
+}
+
+function hexAleatorio(){
+	var random = aletaorio(0, 16777215)
+	if (random < 0){
+	  random = 0xFFFFFFFF + random + 1
+	}
+	return random.toString(16).toUpperCase()
+}
+
+function enArreglo(arreglo, valor) {
+	var posicion = arreglo.indexOf(valor)
+	var rpta = false
+	if(posicion != -1){
+		rpta = true
+	}
+	return rpta
+}
+
+function generarAleatorios(filas){
+  var arreglo = [];
+	for (var i = 0; i < filas; i++) {
+		var continuar = true;
+		while(continuar == true){
+			var temp = aletaorio(1, 151);
+			if(enArreglo(arreglo, temp) == false){
+				continuar = false;
+				arreglo.push(temp);
+			}
+		}
+  }
+  return arreglo
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(cors())
@@ -11,5 +47,25 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/hello', function(req, res) {
     res.send('hello world');
+  })
+  .get('/generator', function(req, res) {
+    var filas = req.query.filas
+    var borde = Boolean(req.query.borde)
+    var relleno = Boolean(req.query.relleno)
+    var rpta = {
+      aleatorios: null, 
+      borde: null,
+      relleno: null,
+    }
+    if (filas <= 12){
+      rpta.aleatorios = generarAleatorios(filas)
+      if (borde){
+        rpta.borde = hexAleatorio()
+      }
+      if (relleno){
+        rpta.relleno = hexAleatorio()
+      }
+    }
+    res.send(JSON.stringify(rpta))
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
